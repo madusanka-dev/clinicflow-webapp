d432e43971def32adcb107b17afc07f44b596ef3
Madusanka <madujayasooriya93@gmail.com>
2026-04-19 12:51:09 +0530

    changes

--- src/features/patient/BookAppointment.tsx
+++ src/features/patient/BookAppointment.tsx
@@ -5,7 +5,6 @@ import api from '@/lib/api'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import type { Slot } from '@/types'
 import { AlertCircle, CheckCircle, Loader2, CalendarIcon  } from 'lucide-react'
 
